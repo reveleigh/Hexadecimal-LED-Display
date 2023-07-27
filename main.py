ab3da5d2bf06b4272eaa01cae0ff852190e604f9
Russell Eveleigh <russelleveleigh@gmail.com>
2023-07-27 17:52:53 +0100

    Capitalised globals and constants and removed duplicate colors

--- main.py
+++ main.py
@@ -16,82 +16,81 @@ SSID = "Hexadecimal Clock"
 PASSWORD = "123456789"
 
 # Define an access point, name it and then make it active
-ap = network.WLAN(network.AP_IF)
-ap.config(essid=SSID, password=PASSWORD)
-ap.active(True)
+AP = network.WLAN(network.AP_IF)
+AP.config(essid=SSID, password=PASSWORD)
+AP.active(True)
 
 # Wait until it is active
-while ap.active == False:
+while AP.active == False:
     pass
 
 print("Access point active")
 # Print out IP information
-print(ap.ifconfig())
+print(AP.ifconfig())
 
 
 # Setting up Neopixel object
-numpix = 136
-strip = Neopixel(numpix, 0, 22, "GRB")
-red = (255, 0, 0)
-off = (0,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-white = (255, 255, 255)
-orange = (255, 50, 0)
-strip.brightness(100)
+NUMPIX = 136
+STRIP = Neopixel(NUMPIX, 0, 22, "GRB")
+
+OFF = (0,0,0)
+WHITE = (255, 255, 255)
+
+# Spectrum colours
+RED = (255, 0, 0)
+ORANGE = (255, 50, 0)
+YELLOW = (255, 100, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+INDIGO = (100, 0, 90)
+VIOLET = (200, 0, 100)
+STRIP.brightness(100)
 
 def rainbow():
-    red = (255, 0, 0)
-    orange = (255, 50, 0)
-    yellow = (255, 100, 0)
-    green = (0, 255, 0)
-    blue = (0, 0, 255)
-    indigo = (100, 0, 90)
-    violet = (200, 0, 100)
-    colors_rgb = [red, orange, yellow, green, blue, indigo, violet]
+    colors_rgb = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET]
 
     colors = colors_rgb
  
-    step = round(numpix / len(colors))
+    step = round(NUMPIX / len(colors))
     current_pixel = 0
 
     for color1, color2 in zip(colors, colors[1:]):
-        strip.set_pixel_line_gradient(current_pixel, current_pixel + step, color1, color2)
+        STRIP.set_pixel_line_gradient(current_pixel, current_pixel + step, color1, color2)
         current_pixel += step
 
-    strip.set_pixel_line_gradient(current_pixel, numpix - 1, violet, red)
+    STRIP.set_pixel_line_gradient(current_pixel, NUMPIX - 1, VIOLET, RED)
     
     start_time = time.time()  # record the start time
-    total_time = 10  # set the total time allowed in seconds
+    total_time = 30  # set the total time allowed in seconds
 
     while (time.time() - start_time) < total_time:
-        strip.rotate_right(1)
+        STRIP.rotate_right(1)
         time.sleep(0.042)
-        strip.show()
+        STRIP.show()
 
-    for i in range(numpix):
-        strip.set_pixel(i, off)       
-    strip.show()
+    for i in range(NUMPIX):
+        STRIP.set_pixel(i, OFF)       
+    STRIP.show()
 
 # turn off all leds
 def turnOff():
-    for i in range(numpix):
-        strip.set_pixel(i, off)
+    for i in range(NUMPIX):
+        STRIP.set_pixel(i, OFF)
         time.sleep(0.1)
         print("Light ", i, " is off" )
-        strip.show()
+        STRIP.show()
 
 # Run through all leds
 def turnOn():
     for i in range(len(led_matrix)):
         for j in range(len(led_matrix[i])):
-            strip.set_pixel(led_matrix[i][j], white)
+            STRIP.set_pixel(led_matrix[i][j], WHITE)
             time.sleep(0.005)
             print("Light ", i, " is on" )
-            strip.show()
+            STRIP.show()
 #turnOn()
 
-place = [0, 0, 0, 0, 0, 0]
+place = [15, 15, 15, 0, 0, 0]
 
 def increment_place(index,base):
     if place[index] < base:
@@ -104,48 +103,49 @@ def displayHex(base):
     while True:
         reverse_place = place[::-1]
         for i in range(136):
-            strip.set_pixel(i, red)
+            STRIP.set_pixel(i, RED)
         for i in range(6):
             for x in range(base+1, 16):
-                strip.set_pixel(led_matrix[i][x], off)
+                STRIP.set_pixel(led_matrix[i][x], OFF)
 
         for i, p in enumerate(reverse_place):
-            strip.set_pixel(led_matrix[i][p], white)
+            STRIP.set_pixel(led_matrix[i][p], WHITE)
 
-        strip.show()
-        time.sleep(0.1)
+        STRIP.show()
+        time.sleep(0.9)
 
         # Increment the place values starting from the last element
         increment_place(len(place) - 1,base)
         if place == [base, base, base, base, base, base]:
             for i in range(base):
-                strip.set_pixel(led_matrix[0][i],red)
-            strip.set_pixel(led_matrix[0][base], white)
-            strip.show()
+                STRIP.set_pixel(led_matrix[0][i],RED)
+            STRIP.set_pixel(led_matrix[0][base], WHITE)
+            STRIP.show()
             print("Got here")
             time.sleep(1)
             rainbow()
             break
 
-displayHex(1)
+while True:
+    rainbow()
 
 def getPlace(r, g, b):
     for i in range(96):
-        strip.set_pixel(i, off)
+        STRIP.set_pixel(i, OFF)
 
     place = [r // 16, r % 16, g // 16, g % 16, b // 16, b % 16]
 
     for i in range(96, 136):
-        strip.set_pixel(i, (r, g, b))
+        STRIP.set_pixel(i, (r, g, b))
 
-    for idx, color in enumerate([red, red, green, green, blue, blue]):
+    for idx, color in enumerate([RED, RED, GREEN, GREEN, BLUE, BLUE]):
         for i in range(place[idx]):
-            strip.set_pixel(led_matrix[5 - idx][i], color)
+            STRIP.set_pixel(led_matrix[5 - idx][i], color)
 
-    for idx, color in enumerate([white, white, white, white, white, white]):
-        strip.set_pixel(led_matrix[idx][place[5 - idx]], color)
+    for idx, color in enumerate([WHITE, WHITE, WHITE, WHITE, WHITE, WHITE]):
+        STRIP.set_pixel(led_matrix[idx][place[5 - idx]], color)
 
-    strip.show()
+    STRIP.show()
     print(place)
 
 
@@ -169,17 +169,17 @@ def cycle_through_spectrum(interval):
       
         
         time.sleep(interval)
-#cycle_through_spectrum(1)
+#cycle_through_spectrum(0.1)
 
-color = [0, 0, 0]
+COLOR = [0, 0, 0]
 def find_colour(r, g, b):
-    global color
+    global COLOR
 
-    while color != [r, g, b]:
+    while COLOR != [r, g, b]:
         for i in range(3):
-            if color[i] != [r, g, b][i]:
-                color[i] += 1 if [r, g, b][i] > color[i] else -1
-        getPlace(*color)
+            if COLOR[i] != [r, g, b][i]:
+                COLOR[i] += 1 if [r, g, b][i] > COLOR[i] else -1
+        getPlace(*COLOR)
 
 #find_colour(255,0,0)
 #time.sleep(2)  
@@ -222,6 +222,7 @@ async def index(request, response):
 
 # Run the web server as the sole process
 app.run(host="0.0.0.0", port=80)
+
 
 
 
